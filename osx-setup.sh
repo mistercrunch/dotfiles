5c319b7f27a881eaf0c811b39911532d4b707489
Maxime Beauchemin <maximebeauchemin@gmail.com>
2025-07-28 21:34:20 -0700

    cleanup

--- osx-setup.sh
+++
@@ -1,5 +0,0 @@
-# from https://github.com/magicmonty/bash-git-prompt
-brew install bash-git-prompt
-
-# vundle (package manager for vim)
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
